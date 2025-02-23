94761931794cc64b8d93dc5364d776d7cd551146
alexpilk <alex.pilkevych@gmail.com>
2025-02-23 13:05:19 +0100

    🍏 Implement /add_batch values length limit

--- tracker/main.py
+++ tracker/main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI
-
-from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.exception_handlers import request_validation_exception_handler
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel, conlist
 
 
 class Batch(BaseModel):
     symbol: str
-    values: list[float]
+    values: conlist(float, max_length=10000)
 
 
 app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def hide_input_value_handler(request: Request, exc: RequestValidationError):
+    """
+    This handler hides the over-sized input list from the error response to ease the load on the network.
+    """
+    errors = exc.errors()
+
+    for error in errors:
+        if error['type'] == 'too_long' and error['loc'] == ('body', 'values'):
+            del error['input']
+
+    return await request_validation_exception_handler(request, exc)
 
 
 @app.post('/add_batch/', status_code=201)
